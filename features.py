f85623e0a5b03a475a1b234663693763ea85b257
Aidan Acquah <36295401+aidanacquah@users.noreply.github.com>
2023-07-22 08:36:01 +0100

    Feature list (#12)
    * Update features to those found in stepcount
    
    * Add documentation of extracted features

--- features.py
+++ features.py
@@ -1,142 +1,172 @@
 import numpy as np
 import scipy.stats as stats
 import scipy.signal as signal
+from scipy.ndimage import median_filter
+import statsmodels.tsa.stattools as stattools
+
+
+MIN_WINDOW_SEC = 2  # seconds
 
 
 def extract_features(xyz, sample_rate=100):
     ''' Extract commonly used HAR time-series features. xyz is a window of shape (N,3) '''
 
+    if np.isnan(xyz).any():
+        return {}
+
+    if len(xyz) <= MIN_WINDOW_SEC * sample_rate:
+        return {}
+
     feats = {}
 
-    x, y, z = xyz.T
-
-    feats['xmin'], feats['xq25'], feats['xmed'], feats['xq75'], feats['xmax'] = np.quantile(x, (0, .25, .5, .75, 1))
-    feats['ymin'], feats['yq25'], feats['ymed'], feats['yq75'], feats['ymax'] = np.quantile(y, (0, .25, .5, .75, 1))
-    feats['zmin'], feats['zq25'], feats['zmed'], feats['zq75'], feats['zmax'] = np.quantile(z, (0, .25, .5, .75, 1))
-
-    with np.errstate(divide='ignore', invalid='ignore'):  # ignore div by 0 warnings
-        # xy, xy, zx correlation
-        feats['xycorr'] = np.nan_to_num(np.corrcoef(x, y)[0, 1])
-        feats['yzcorr'] = np.nan_to_num(np.corrcoef(y, z)[0, 1])
-        feats['zxcorr'] = np.nan_to_num(np.corrcoef(z, x)[0, 1])
-
     v = np.linalg.norm(xyz, axis=1)
+    v = median_filter(v, size=5, mode='nearest')
+    v = v - 1  # detrend: "remove gravity"
+    v = np.clip(v, -2, 2)  # clip abnormaly high values
 
-    feats['min'], feats['q25'], feats['med'], feats['q75'], feats['max'] = np.quantile(v, (0, .25, .5, .75, 1))
+    # Moments features
+    feats.update(moments_features(v, sample_rate))
 
-    with np.errstate(divide='ignore', invalid='ignore'):  # ignore div by 0 warnings
-        # 1s autocorrelation
-        feats['corr1s'] = np.nan_to_num(np.corrcoef(v[:-sample_rate], v[sample_rate:]))[0, 1]
+    # Quantile features
+    feats.update(quantile_features(v, sample_rate))
 
-    # Angular features
-    feats.update(angular_features(xyz, sample_rate))
+    # Autocorrelation features
+    feats.update(autocorr_features(v, sample_rate))
 
     # Spectral features
     feats.update(spectral_features(v, sample_rate))
 
+    # FFT features
+    feats.update(fft_features(v, sample_rate))
+
     # Peak features
-    feats.update(peak_features(v, sample_rate))
+    feats.update(peaks_features(v, sample_rate))
+
+    return feats
+
+
+def moments_features(v, sample_rate=None):
+    """ Moments """
+    avg = np.mean(v)
+    std = np.std(v)
+    if std > .01:
+        skew = np.nan_to_num(stats.skew(v))
+        kurt = np.nan_to_num(stats.kurtosis(v))
+    else:
+        skew = kurt = 0
+    feats = {
+        'avg': avg,
+        'std': std,
+        'skew': skew,
+        'kurt': kurt,
+    }
+    return feats
+
+
+def quantile_features(v, sample_rate=None):
+    """ Quantiles (min, 25th, med, 75th, max) """
+    feats = {}
+    feats['min'], feats['q25'], feats['med'], feats['q75'], feats['max'] = np.quantile(v, (0, .25, .5, .75, 1))
+    return feats
+
+
+def autocorr_features(v, sample_rate):
+    """ Autocorrelation features """
+
+    with np.errstate(divide='ignore', invalid='ignore'):  # ignore invalid div warnings
+        u = np.nan_to_num(stattools.acf(v, nlags=2 * sample_rate))
+
+    peaks, _ = signal.find_peaks(u, prominence=.1)
+    if len(peaks) > 0:
+        acf_1st_max_loc = peaks[0]
+        acf_1st_max = u[acf_1st_max_loc]
+        acf_1st_max_loc /= sample_rate  # in secs
+    else:
+        acf_1st_max = acf_1st_max_loc = 0.0
+
+    valleys, _ = signal.find_peaks(-u, prominence=.1)
+    if len(valleys) > 0:
+        acf_1st_min_loc = valleys[0]
+        acf_1st_min = u[acf_1st_min_loc]
+        acf_1st_min_loc /= sample_rate  # in secs
+    else:
+        acf_1st_min = acf_1st_min_loc = 0.0
+
+    acf_zeros = np.sum(np.diff(np.signbit(u)))
+
+    feats = {
+        'acf_1st_max': acf_1st_max,
+        'acf_1st_max_loc': acf_1st_max_loc,
+        'acf_1st_min': acf_1st_min,
+        'acf_1st_min_loc': acf_1st_min_loc,
+        'acf_zeros': acf_zeros,
+    }
 
     return feats
 
 
 def spectral_features(v, sample_rate):
-    """ Spectral entropy, 1st & 2nd dominant frequencies """
+    """ Spectral entropy, average power, dominant frequencies """
 
     feats = {}
 
-    # Spectrum using Welch's method with 3s segment length
-    # First run without detrending to get the true spectrum
-    freqs, powers = signal.welch(v, fs=sample_rate,
-                                 nperseg=3 * sample_rate,
-                                 noverlap=2 * sample_rate,
-                                 detrend=False,
-                                 average='median')
+    freqs, powers = signal.periodogram(v, fs=sample_rate, detrend='constant', scaling='density')
+    powers /= (len(v) / sample_rate)    # unit/sec
 
-    with np.errstate(divide='ignore', invalid='ignore'):  # ignore div by 0 warnings
-        feats['pentropy'] = np.nan_to_num(stats.entropy(powers + 1e-16))
-
-    # Spectrum using Welch's method with 3s segment length
-    # Now do detrend to focus on the relevant freqs
-    freqs, powers = signal.welch(v, fs=sample_rate,
-                                 nperseg=3 * sample_rate,
-                                 noverlap=2 * sample_rate,
-                                 detrend='constant',
-                                 average='median')
+    feats['pentropy'] = stats.entropy(powers[powers > 0])
+    feats['power'] = np.sum(powers)
 
     peaks, _ = signal.find_peaks(powers)
     peak_powers = powers[peaks]
     peak_freqs = freqs[peaks]
     peak_ranks = np.argsort(peak_powers)[::-1]
-    if len(peaks) >= 2:
-        feats['f1'] = peak_freqs[peak_ranks[0]]
-        feats['f2'] = peak_freqs[peak_ranks[1]]
-        feats['p1'] = peak_powers[peak_ranks[0]]
-        feats['p2'] = peak_powers[peak_ranks[1]]
-    elif len(peaks) == 1:
-        feats['f1'] = feats['f2'] = peak_freqs[peak_ranks[0]]
-        feats['p1'] = feats['p2'] = peak_powers[peak_ranks[0]]
-    else:
-        feats['f1'] = feats['f2'] = 0
-        feats['p1'] = feats['p2'] = 0
+
+    TOPN = 3
+    feats.update({f"f{i + 1}": 0 for i in range(TOPN)})
+    feats.update({f"p{i + 1}": 0 for i in range(TOPN)})
+    for i, j in enumerate(peak_ranks[:TOPN]):
+        feats[f"f{i + 1}"] = peak_freqs[j]
+        feats[f"p{i + 1}"] = peak_powers[j]
 
     return feats
 
 
-def peak_features(v, sample_rate):
-    """ Features of the signal peaks. A proxy to step counts. """
+def fft_features(v, sample_rate, nfreqs=5):
+    """ Power of frequencies 0Hz, 1Hz, 2Hz, ... using Welch's method """
+
+    _, powers = signal.welch(
+        v, fs=sample_rate,
+        nperseg=sample_rate,
+        noverlap=sample_rate // 2,
+        detrend='constant',
+        scaling='density',
+        average='median'
+    )
+
+    feats = {f"fft{i}": powers[i] for i in range(nfreqs + 1)}
+
+    return feats
+
+
+def peaks_features(v, sample_rate):
+    """ Features of the signal peaks """
 
     feats = {}
-    u = butterfilt(v, (.6, 5), fs=sample_rate)
+    u = butterfilt(v, 5, fs=sample_rate)  # lowpass 5Hz
     peaks, peak_props = signal.find_peaks(u, distance=0.2 * sample_rate, prominence=0.25)
-    feats['numPeaks'] = len(peaks)
+    feats['npeaks'] = len(peaks) / (len(v) / sample_rate)  # peaks/sec
     if len(peak_props['prominences']) > 0:
-        feats['peakPromin'] = np.median(peak_props['prominences'])
+        feats['peaks_avg_promin'] = np.mean(peak_props['prominences'])
+        feats['peaks_min_promin'] = np.min(peak_props['prominences'])
+        feats['peaks_max_promin'] = np.max(peak_props['prominences'])
     else:
-        feats['peakPromin'] = 0
+        feats['peaks_avg_promin'] = feats['peaks_min_promin'] = feats['peaks_max_promin'] = 0
 
     return feats
 
 
-def angular_features(xyz, sample_rate):
-    """ Roll, pitch, yaw.
-    Hip and Wrist Accelerometer Algorithms for Free-Living Behavior
-    Classification, Ellis et al.
-    """
-
-    feats = {}
-
-    # Raw angles
-    x, y, z = xyz.T
-
-    roll = np.arctan2(y, z)
-    pitch = np.arctan2(x, z)
-    yaw = np.arctan2(y, x)
-
-    feats['avgroll'] = np.mean(roll)
-    feats['avgpitch'] = np.mean(pitch)
-    feats['avgyaw'] = np.mean(yaw)
-    feats['sdroll'] = np.std(roll)
-    feats['sdpitch'] = np.std(pitch)
-    feats['sdyaw'] = np.std(yaw)
-
-    # Gravity angles
-    xyz = butterfilt(xyz, 0.5, fs=sample_rate)
-
-    x, y, z = xyz.T
-
-    roll = np.arctan2(y, z)
-    pitch = np.arctan2(x, z)
-    yaw = np.arctan2(y, x)
-
-    feats['rollg'] = np.mean(roll)
-    feats['pitchg'] = np.mean(pitch)
-    feats['yawg'] = np.mean(yaw)
-
-    return feats
-
-
-def butterfilt(x, cutoffs, fs, order=10, axis=0):
+def butterfilt(x, cutoffs, fs, order=4, axis=0):
+    """ Butterworth filter """
     nyq = 0.5 * fs
     if isinstance(cutoffs, tuple):
         hicut, lowcut = cutoffs
@@ -157,5 +187,5 @@ def butterfilt(x, cutoffs, fs, order=10, axis=0):
 def get_feature_names():
     """ Hacky way to get the list of feature names """
 
-    feats = extract_features(np.zeros((1000, 3)), 100)
+    feats = extract_features(np.zeros((500, 3)), 100)
     return list(feats.keys())
